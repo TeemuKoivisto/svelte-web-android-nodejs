8ceab16cc17de71f3f61579ed9af02cfc0ff72f1
Teemu Koivisto <tkoivisto456@gmail.com>
2025-07-30 14:44:35 +0300

    move handle completely into handler

--- packages/api/src/lib/handlers.ts
+++ packages/api/src/lib/handlers.ts
@@ -17,42 +17,11 @@ type InferResponse<K extends keyof typeof routes> = (typeof routes)[K] extends {
   ? z.infer<(typeof routes)[K]['response']>
   : any
 
-// export const parseQuery = <T extends z.ZodTypeAny>(searchParams: URLSearchParams, schema: T) => {
-//   const intoObject = searchParams.entries().reduce<Record<string, string>>((acc, cur) => {
-//     acc[cur[0]] = cur[1]
-//     return acc
-//   }, {})
-//   return schema.safeParse(intoObject) as z.SafeParseReturnType<z.infer<T>, z.infer<T>>
-// }
-
-type ParsedBody<K extends keyof typeof routes> =
-  | z.SafeParseReturnType<any, InferBody<K>>
-  | undefined
-type ParsedQuery<K extends keyof typeof routes> =
-  | z.SafeParseReturnType<any, InferQuery<K>>
-  | undefined
-
-const searchIntoObject = (params: URLSearchParams) =>
+export const searchIntoObject = (params: URLSearchParams) =>
   params.entries().reduce<Record<string, string>>((acc, cur) => {
     acc[cur[0]] = cur[1]
     return acc
   }, {})
-
-export const handle =
-  (event: RequestEvent) =>
-  async <K extends keyof typeof routes>(
-    key: K
-  ): Promise<[ParsedBody<K>, z.SafeParseReturnType<any, InferQuery<K>> | undefined]> => {
-    const handler = routes[key]
-    return [
-      'body' in handler
-        ? (handler.body.safeParse(await event.request.json()) as ParsedBody<K>)
-        : undefined,
-      'query' in handler
-        ? (handler.query.safeParse(searchIntoObject(event.url.searchParams)) as ParsedQuery<K>)
-        : undefined
-    ]
-  }
 
 export const handler =
   <K extends keyof typeof routes>(
@@ -64,14 +33,19 @@ export const handler =
     ) => Promise<Result<InferResponse<K>>>
   ) =>
   async (event: RequestEvent): Promise<Response> => {
-    const [body, query] = await handle(event)(key)
+    const handler = routes[key]
+    const body = 'body' in handler ? handler.body.safeParse(await event.request.json()) : undefined
+    const query =
+      'query' in handler
+        ? handler.query.safeParse(searchIntoObject(event.url.searchParams))
+        : undefined
     if (body?.error || query?.error) {
       // @TODO show generic errors in prod?
       return json(body?.error || query?.error, {
         status: 400
       })
     }
-    const result = await fn(event, body?.data, query?.data)
+    const result = await fn(event, body?.data as InferBody<K>, query?.data as InferQuery<K>)
     if ('err' in result) {
       return error(result.code, result.err)
     }
--- packages/api/src/routes/oauth/github/callback/+server.ts
+++ packages/api/src/routes/oauth/github/callback/+server.ts
@@ -1,9 +1,17 @@
-import { error } from '@sveltejs/kit'
-import { handle } from '$lib/handlers'
+import { json } from '@sveltejs/kit'
+import { searchIntoObject } from '$lib/handlers'
 
 import type { RequestHandler } from './$types'
+import { oauth } from '@org/lib/schemas'
 
 export const GET: RequestHandler = async event => {
-  const { query } = await handle(event)('GET /oauth/github/callback')
-  return Response.redirect(`${query.redirect_uri}?location=${query.location}`, 301)
+  const query = oauth['GET /oauth/github/callback'].query.safeParse(
+    searchIntoObject(event.url.searchParams)
+  )
+  if (query.error) {
+    return json(query.error, {
+      status: 400
+    })
+  }
+  return Response.redirect(`${query.data.redirect_uri}?location=${query.data.location}`, 301)
 }
